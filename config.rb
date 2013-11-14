1be4222a71ca85cdc8c729ed6da07e077da67cdb
Adriano Fernandes da Silva <adrianof@lt-mbp-sp-022.ny.rga.com>
2013-11-14 18:03:12 -0200

    new structure

--- config.rb
+++ config.rb
@@ -14,29 +14,6 @@ activate :livereload
 # Helpers
 # ---------------------------------
 helpers do
-  # method for get stylesheets and javascripts of template
-  def path_assets
-    arr_classes = page_classes.split( ' ' )
-
-    if arr_classes.length < 2
-      arr_classes[ 0 ]
-    else
-
-      arr_section = arr_classes[ ( arr_classes.length - 1 ) ].split( '_' )
-      asset_path  = ''
-
-      arr_section.each_with_index do |lista, i|
-        if i == ( arr_section.length - 1 )
-          asset_path = asset_path + lista
-        else
-          asset_path = asset_path + lista + '/'
-        end
-      end
-
-      asset_path
-
-    end
-  end
 end
 
 # ---------------------------------
---
+++ source/public/javascripts/pages/home.js
@@ -0,0 +1,19 @@
+// -----------------------------------------------
+// Import Shared
+// -----------------------------------------------
+
+//= require '_shared/app'
+
+// -----------------------------------------------
+// Import Plugins
+// -----------------------------------------------
+
+//= require '_plugins/carousel.min'
+//= require '_plugins/jquery.fancybox'
+
+// -----------------------------------------------
+// Import Components
+// -----------------------------------------------
+
+//= require '_components/carousel'
+//= require '_components/lightbox'
