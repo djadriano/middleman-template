c264ef403acef8e305fe2a8caff3094ddfce1a21
Adriano Fernandes <djadrianof@gmail.com>
2013-11-28 22:51:20 -0200

    updated structure

--- source/public/javascripts/pages/home.js
+++ source/public/javascripts/pages/home.js
@@ -2,18 +2,12 @@
 // Import Shared
 // -----------------------------------------------
 
-//= require '_shared/app'
 
 // -----------------------------------------------
 // Import Plugins
 // -----------------------------------------------
 
-//= require '_plugins/carousel.min'
-//= require '_plugins/jquery.fancybox'
 
 // -----------------------------------------------
 // Import Components
 // -----------------------------------------------
-
-//= require '_components/carousel'
-//= require '_components/lightbox'
